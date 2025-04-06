91c497f181d2681d160054802b61b8b00f8e2e52
Paul Denlinger <pmdenlinger@icloud.com>
2025-04-06 13:12:18 -0700

    Function exercises

--- js-basics-exercises/challenges/05-functions.js
+++ js-basics-exercises/challenges/05-functions.js
@@ -12,16 +12,26 @@
 
 */
 
-const calculateTotal = function random(subTotal, tax) {
-  // const total = subTotal + tax;
-  // return total;
-  return subTotal + tax;
-};
+// const calculateTotal = function random(subTotal, tax) {
+//   // const total = subTotal + tax;
+//   // return total;
+//   return subTotal + tax;
+
 
 // console.log(calculateTotal(200, 20));
 
-const order1 = calculateTotal(100, 10);
-const order2 = calculateTotal(50, 5);
-const order3 = calculateTotal(25, 5);
+// const order1 = calculateTotal(100, 10);
+// const order2 = calculateTotal(50, 5);
+// const order3 = calculateTotal(25, 5);
+
+// console.log(order1, order2, order3);
+
+function calculateTotal(subTotal, tax) {
+  return subTotal + tax;
+}
+
+const order1 = calculateTotal(100, 12)
+const order2 = calculateTotal(50, 6);
+const order3 = calculateTotal(30, 5);
 
 console.log(order1, order2, order3);
