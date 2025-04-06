35a2b10c1fb84df79f4ef8a06eab1e756f11ff06
Paul Denlinger <pmdenlinger@icloud.com>
2025-04-06 12:32:23 -0700

    Numbers practice

---
+++ js-basics-exercises/challenges/01-vars.js
@@ -0,0 +1,26 @@
+/*
+## Variables #1
+
+1. create "firstName" and "last_name" variables
+2. assign your values
+3. create "address" variable and assign "main street" value to it
+4. re-assign address to "first street" later
+5. log all values in the console
+
+*/
+
+// const firstName = "john";
+// const last_name = "smilga";
+
+// let address = "main street";
+// address = "first street";
+
+// console.log(firstName, last_name, address);
+
+const firstName = "Paul";
+const last_name = "Denlinger";
+
+let address = "main street";
+address = "first street";
+
+console.log(firstName, last_name, address);
